3a57bb943647caed2f0c01f9acacf92f9d96696a
Shovon Hasan <im.shovonhasan@gmail.com>
2016-07-15 12:08:07 -0400

    Fixed misplaced hashtag, need to fix script not making a new file for the pictures

--- save_subreddit_images.rb
+++ save_subreddit_images.rb
@@ -1,27 +1,27 @@
 require 'mechanize'
 
-class TestMech < Mechanize
-  def process
-    url= 'http://imgur.com/r/aww'
-    get url
+class ImgurScraper < Mechanize
+  def save_images(url)
+    page = get(url)
     directory = url[/\/r\/(.*?)\//, 1]
 
-    thumbnails = page.images_with(:src => /i.imgur.com\/[[:alnum:]]{4,10}/)
+    thumbnails = page.images_with(:src => /i.imgur.com\/[[:alnum:]]{6,10}/)
     thumbnails.each do |link|
       begin
         transact do
+          #puts "Code has made it this far"
           agent = Mechanize.new
           page = agent.get(link.to_s.chomp('.jpg')) # use thumbnail URL to go to full image
-          image = page.image_with(:src => /i.imgur.com\/[[:alnum:]]{4,10}/).fetch
+          image = page.image_with(:src => /i.imgur.com\/[[:alnum:]]{6,10}/).fetch
           save_name = link.to_s.split('/')[-1]
-          image.save! "#E:\\Users\\Pictures\\{directory}\\#{save_name}"
+          image.save! "E:\\Users\\Pictures\\test_directory\\#{directory}\\#{save_name}"
         end
       rescue
         $stderr.puts link
       end
     end
   end
-end
+  #self
+end#.new.save_images(ARGV[0])
+ImgurScraper.new.save_images("http://imgur.com/r/aww")
 
-page = TestMech.new
-page.process
