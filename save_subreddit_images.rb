b9c104c91d5d406c7f06be5ccd5eaa816901370e
Shovon Hasan <im.shovonhasan@gmail.com>
2016-07-15 12:56:50 -0400

    Added a way to specify URL from command line and added an unsophisticated way to format the URL

--- save_subreddit_images.rb
+++ save_subreddit_images.rb
@@ -2,26 +2,40 @@ require 'mechanize'
 
 class ImgurScraper < Mechanize
   def save_images(url)
-    page = get(url)
-    directory = url[/\/r\/(.*?)\//, 1]
+    @url = sanitize_input(url.dup)
+
+    get @url
+    directory = @url[/\/r\/(.*?)\//, 1]
 
     thumbnails = page.images_with(:src => /i.imgur.com\/[[:alnum:]]{6,10}/)
     thumbnails.each do |link|
       begin
         transact do
-          #puts "Code has made it this far"
           agent = Mechanize.new
           page = agent.get(link.to_s.chomp('.jpg')) # use thumbnail URL to go to full image
           image = page.image_with(:src => /i.imgur.com\/[[:alnum:]]{6,10}/).fetch
           save_name = link.to_s.split('/')[-1]
-          image.save! "E:\\Users\\Pictures\\test_directory\\#{directory}\\#{save_name}"
+          image.save! "E:\\Users\\Pictures\\#{directory}\\#{save_name}"
         end
       rescue
         $stderr.puts link
       end
     end
   end
-  #self
-end#.new.save_images(ARGV[0])
-ImgurScraper.new.save_images("http://imgur.com/r/aww")
+
+  private
+  def sanitize_input(url)
+    abort("ERROR: URL is not a string.") unless url.is_a?(String)
+
+    url.insert(0, 'http://') unless url[0..6] == 'http://'
+
+    if url[-1, 1] != "/"
+      url << "/"
+    end
+
+    return url
+  end
+end
+
+ImgurScraper.new.save_images(ARGV[0])
 
