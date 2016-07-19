a487893b95b468b9a0b7c3d2542005a5d97929bc
Shovon Hasan <im.shovonhasan@gmail.com>
2016-07-19 10:53:31 -0400

    small file to decide which script to run based on URL

---
+++ save_images.rb
@@ -0,0 +1,24 @@
+require_relative '4chan_save_images'
+require_relative 'save_subreddit_images'
+
+# ARGV.each do |a|
+#   case a
+#   when a.include?('imgur.com')
+#     ImgurScraper.new.save_images(ARGV[0])
+#   when a.include?('4chan.org')
+#     ChanScraper.new.save_images(ARGV[0])
+#   else
+#     puts "Invalid URL not site not supported"
+#   end
+# end
+
+ARGV.each do |a|
+  case a
+  when /imgur.com\/r\//
+    ImgurSubScraper.new.save_images(ARGV[0])
+  when /4chan.org/
+    ChanScraper.new.save_images(ARGV[0])
+  else
+    puts "Invalid URL not site not supported"
+  end
+end
