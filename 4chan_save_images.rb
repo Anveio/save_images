8902994b6e61694fab4b008685303dd9df4374df
Shovon Hasan <im.shovonhasan@gmail.com>
2016-07-19 10:54:17 -0400

    removed last line, improved documentation

--- 4chan_save_images.rb
+++ 4chan_save_images.rb
@@ -1,5 +1,4 @@
 require 'mechanize'
-require 'logger'
 
 class ChanScraper < Mechanize
   def save_images(url)
@@ -10,7 +9,7 @@ class ChanScraper < Mechanize
 
     thread = unsecure_page
     thread = thread.get(@url)
-    puts "Connection Established, downloading..."
+    puts "Connection Established, downloading thread"
 
     file_name = thread.links_with(href: /i.4cdn.org\/#{@board_name}\/[[:alnum:]]{11,14}/)
     file_name.each do |link|
@@ -24,16 +23,21 @@ class ChanScraper < Mechanize
   private
   def sanitize_url(url)
     abort("ERROR: URL is not a string.") unless url.is_a?(String)
-
+    abort("ERROR: Invalid 4chan URL.") unless url =~ /4chan.org/
+    #Mechanize needs "http" in order to retrieve a page
     url.insert(0, 'https://') unless url[0..7] == 'https://'
+
+    #Need final character to be a slash because @thread_name and
+    #@board_name count backwards from the last slash
     if url[-1, 1] != "/"
       url << "/"
     end
     return url
   end
 
-  def invalid?(url)
-    if url.include? "http"
+  #prevent file name errors when saving
+  def invalid?(save_name)
+    if save_name.include? "http"
       return true
     end
   end
@@ -46,4 +50,4 @@ class ChanScraper < Mechanize
   end
 end
 
-ChanScraper.new.save_images(ARGV[0])
+#ChanScraper.new.save_images(ARGV[0])
