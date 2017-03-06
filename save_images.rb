fdca43bbb2d27f2340604fb5b08feeb72a666baf
arthanos <im.shovonhasan@gmail.com>
2017-03-06 09:28:07 -0500

    Add support for batch downloads

--- save_images.rb
+++ save_images.rb
@@ -4,11 +4,11 @@ require_relative 'save_subreddit_images'
 ARGV.each do |a|
   case a
   when /imgur.com\/r\//i
-    ImgurSubScraper.new.save_images(ARGV[0])
+    ImgurSubScraper.new.save_images(a)
   when /4chan.org/i
-    ChanScraper.new(ARGV[0]).save_images
+    ChanScraper.new(a).save_images
   when /reddit.com/i
-    RedditScraper.new.save_images(ARGV[0])
+    RedditScraper.new.save_images(a)
   else
     puts "Invalid URL or the site is not supported."
   end
